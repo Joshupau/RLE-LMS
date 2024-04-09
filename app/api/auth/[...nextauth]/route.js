e99d18e299418e6706aa9713a46ee8afd68964e4
Joshupau <Joshuadg511@gmail.com>
2024-04-09 13:32:57 +0800

    try fix build error

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 import bcrypt from "bcrypt";
 import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
